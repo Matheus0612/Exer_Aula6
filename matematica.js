9a6c13a97fdf43b6a8009acbb4fa93f60138cc23
Matheus Ramos <matheus_0612@hotmail.com>
2021-04-15 16:58:47 -0300

    Criação de solicitação e resposta do servidor

---
+++ matematica.js
@@ -0,0 +1,16 @@
+var operations= {
+    soma: function(a,b){
+        return a+b;
+    },
+    subt: function(a,b){
+        return a-b;
+    },
+    mult: function(a,b){
+        return a*b;
+    },
+    div: function(a,b){
+        return a/b;
+    }
+}
+
+module.exports= operations;
